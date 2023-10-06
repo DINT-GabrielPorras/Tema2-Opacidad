02f7f8ee049ce63f433bd0ad89653a0de054c8b0
GABRIEL PORRAS PÉREZ <gabporper@alu.edu.gva.es>
2023-10-06 14:30:57 +0200

    Agregar archivos de proyecto.

---
+++ Opacidad2/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Opacidad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void EpisodioIV_MouseEnter(Object sender, MouseEventArgs e) 
+        {
+            if(sender == episodioIV)
+                episodioIV.Opacity = 1;
+        }
+        public void EpisodioIV_MouseLeave(Object sender, MouseEventArgs e)
+        {
+            if (sender == episodioIV)
+                episodioIV.Opacity = 0.5;
+        }
+        public void EpisodioV_MouseEnter(Object sender, MouseEventArgs e)
+        {
+            if (sender == episodioV)
+                episodioV.Opacity = 1;
+        }
+        public void EpisodioV_MouseLeave(Object sender, MouseEventArgs e)
+        {
+            if (sender == episodioV)
+                episodioV.Opacity = 0.5;
+        }
+        public void EpisodioVI_MouseEnter(Object sender, MouseEventArgs e)
+        {
+            if (sender == episodioVI)
+                episodioVI.Opacity = 1;
+        }
+        public void EpisodioVI_MouseLeave(Object sender, MouseEventArgs e)
+        {
+            if (sender == episodioVI)
+                episodioVI.Opacity = 0.5;
+        }
+
+
+    }
+}
